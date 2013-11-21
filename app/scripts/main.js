99396de8e13fb99138f47f144c5e2325dab61f45
David Graf <davidagraf@gmail.com>
2013-11-21 10:54:03 +0100

    init divs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+(function() {
+  var widgetsHeights = [3, 2, 5, 1, 1, 4, 3, 6, 2, 4, 2, 4, 3, 2, 1],
+      //COLUMN_WIDTH = 200,
+      ROW_HEIGHT = 50,
+      $container = $('#container'),
+      widgets = new Array(widgetsHeights.length);
+
+  $(function() {
+    $.each(widgetsHeights, function(i, v) {
+      var $widget = $('<div class="widget" />');
+      $widget.height(ROW_HEIGHT * v);
+      widgets[i] = {
+        priority: i,
+        pos: 0,
+        dom: $widget
+      };
+      $container.append($widget);
+    });
+    console.log(widgets);
+  });
+
+})();
