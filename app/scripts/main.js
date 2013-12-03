2b1b8f96167c5ae304358db476291b05d0151587
David Graf <davidagraf@gmail.com>
2013-12-03 09:13:35 +0100

    fixed new cols algo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,6 @@
       ROW_HEIGHT = 50,
       $container = $('#container'),
       widgets = new Array(widgetsHeights.length),
-      HORIZONTAL_VERTICAL_COEFF = 1,
       math = mathjs(),
       sumWidgetHeights = 0,
  
@@ -20,76 +19,50 @@
     }
     return nr;
   },
-  nrOfColumns = computeNrOfColumns(),
+  originalNrOfColumns = computeNrOfColumns(),
+  nrOfColumns = originalNrOfColumns,
 
-  positionWidgets = function(updatePrios) {
+  positionWidgets = function(savePosition) {
     var free = new Array(nrOfColumns),
-        widgetsClone = widgets.slice(0),
-        potentialToInsert, widgetToInsert,
-        verticalNormalizer = nrOfColumns / sumWidgetHeights,
-        horizontalNormalizer = 1 / (nrOfColumns - 1),
         max;
 
     console.log('Repositioning with ' + nrOfColumns + ' columns.');
+    console.log('Original: ' + originalNrOfColumns);
 
     $.each(free, function(i) {
-      free[i] = {
-        top: 0,
-      };
+      free[i] = 0;
     });
 
-    while (widgetsClone.length > 0) {
-
-      $.each(free, function(i, v) {
-        v.weight = Infinity;
-      });
-    
-      $.each(free, function(i, v) {
-        var normRight = i * horizontalNormalizer;
-        $.each(widgetsClone, function(j, w) {
-          var weight = w.top * HORIZONTAL_VERTICAL_COEFF +
-                       math.round(math.abs(normRight - w.right), 6); //rounding to avoid rounding error of javascript
-          if (weight < v.weight) {
-            v.weight = weight;
-            v.index = j;
-          }
-        });
-      });
-
-      potentialToInsert = {
-        top: Infinity,
-        weight: Infinity
-      };
-
-      $.each(free, function(i, v) {
-        if (potentialToInsert.weight > v.weight || potentialToInsert.top > v.top) {
-          potentialToInsert = v;
-          potentialToInsert.col = i;
-        }
-      });
-
-      widgetToInsert = widgetsClone.splice(potentialToInsert.index, 1)[0];
-
-      widgetToInsert.dom.css({
-        top: potentialToInsert.top,
-        left: potentialToInsert.col * (COLUMN_WIDTH + MARGIN)
-      });
-
-      if (updatePrios) {
-        widgetToInsert.top = potentialToInsert.top * verticalNormalizer;
-        widgetToInsert.right = potentialToInsert.col * horizontalNormalizer;
-        widgetToInsert.dom.attr('data-col', potentialToInsert.col);
+    $.each(widgets, function(i, widget) {
+      var from, to, j, minCol;
+      if (widget.left < 0) {
+        from = 0;
+        to = nrOfColumns - 1;
+      } else {
+        from = math.floor(widget.left * nrOfColumns / originalNrOfColumns);
+        to = math.ceil((widget.left + 1) * nrOfColumns / originalNrOfColumns) - 1;
       }
 
-      $('.info', widgetToInsert.dom).text(
-        'top: ' + math.round(widgetToInsert.top, 2) +
-        ' / right: ' + math.round(widgetToInsert.right, 2) +
-        ' / weight: ' + math.round(potentialToInsert.weight, 2)
-      );
+      minCol = from;
+      for (j = from + 1; j <= to; ++j) {
+        if (free[j] < free[minCol]) {
+          minCol = j;
+        }
+      }
 
-      potentialToInsert.top += (MARGIN + widgetToInsert.dom.height());
+      widget.dom.css({
+        top: free[minCol],
+        left: minCol * (COLUMN_WIDTH + MARGIN)
+      });
 
-    }
+      if (savePosition) {
+        widget.top = free[minCol];
+        widget.left = minCol;
+        widget.dom.attr('data-col', minCol);
+      }
+
+      free[minCol] += (MARGIN + widget.dom.height());
+    });
 
     max = -1;
     $.each(free, function(i, v) {
@@ -127,7 +100,7 @@
       $widget.height(ROW_HEIGHT * v);
       widgets[i] = {
         top: 0,
-        right: 0,
+        left: -1,
         dom: $widget
       };
 
